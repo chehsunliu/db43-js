4f922ba8c2485f75da9c4b17d00331f851bcba35
chehsunliu <chehsunliu@gmail.com>
2024-04-17 01:03:10 +0800

    Embed settings for MySQL

--- pkgs/db43-test/src/__tests__/setup-mysql.ts
+++ pkgs/db43-test/src/__tests__/setup-mysql.ts
@@ -4,26 +4,9 @@ import * as db43 from "@chehsunliu/db43";
 import { MySqlPlugin } from "@chehsunliu/db43-mysql";
 import { configure as configureRepo, MySqlPostRepository } from "@chehsunliu/db43-test";
 
-let knex1: Knex.Knex | undefined;
 let knex2: Knex.Knex | undefined;
 
 beforeAll(async () => {
-  knex1 = Knex.knex({
-    client: "mysql2",
-    connection: {
-      host: "127.0.0.1",
-      port: 3306,
-      user: "root",
-      password: "xxx",
-      database: "demo",
-      multipleStatements: true,
-    },
-    pool: {
-      afterCreate: (conn: any, done: any) => {
-        conn.query("SET FOREIGN_KEY_CHECKS = 0", (err: any) => done(err, conn));
-      },
-    },
-  });
   knex2 = Knex.knex({
     client: "mysql2",
     connection: {
@@ -32,24 +15,34 @@ beforeAll(async () => {
       user: "root",
       password: "xxx",
       database: "demo",
-      multipleStatements: true,
-    },
+      multipleStatements: true
+    }
   });
-
-  db43.configure({ plugins: [new MySqlPlugin({ knex: knex1 })] });
   configureRepo(
     new MySqlPostRepository(
       {
         userTableName: "users",
         postTableName: "posts",
-        commentTableName: "comments",
+        commentTableName: "comments"
       },
-      knex2,
-    ),
+      knex2
+    )
   );
+
+  db43.configure({
+    plugins: [new MySqlPlugin({
+      connection: {
+        host: "127.0.0.1",
+        port: 3306,
+        user: "root",
+        password: "xxx",
+        database: "demo"
+      }
+    })]
+  });
 });
 
 afterAll(async () => {
-  await knex1!.destroy();
   await knex2!.destroy();
+  await db43.release();
 });
